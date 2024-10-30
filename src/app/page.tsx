50a5c7474244c21df311e3b85dba3148437a35e4
Anwar-Barakat <brkatanwar0@gmail.com>
2024-10-30 21:05:40 +0400

    Add FeaturesPlanet component and replace PageIllustration; remove old illustration file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import HeroHome from '@/components/hero-hom';
+import FeaturesPlanet from '@/components/pages/home/features-planet';
+import HeroHome from '@/components/pages/home/hero-hom';
 import React from 'react';
 
 const Page = () => {
     return (
         <>
             <HeroHome />
+            <FeaturesPlanet />
         </>
     );
 };
